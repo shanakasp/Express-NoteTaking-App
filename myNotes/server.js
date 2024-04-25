7e6d9c201264026b38ad1986754625675f6a3fe9
shanakasp <74164029+shanakasp@users.noreply.github.com>
2024-04-25 12:42:24 +0530

    modified server.js added notes.ejs

--- myNotes/server.js
+++ myNotes/server.js
@@ -14,9 +14,30 @@ app.get("/time", (req, res) => {
   res.send(`<h1>${Date()}</h1>`);
 });
 
+app.get("/notes", (req, res) => {
+  res.reder("notes.ejs", {
+    notes,
+  });
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
 
 // npm i -D nodemon to install nodemon as a dev dependency
+const notes = [
+  {
+    id: 1,
+    title: "sample test",
+    timestamp: Date.now(),
+    contents: "sample content",
+  },
+
+  {
+    id: 2,
+    title: "sample test w",
+    timestamp: Date.now(),
+    contents: "sample content 2",
+  },
+];
